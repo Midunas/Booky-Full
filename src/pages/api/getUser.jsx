9f655e75a5969a34edaf14ea83ae11559c03f548
Midunas <mykolas.vidunas@codeacademylt.onmicrosoft.com>
2022-11-14 19:19:37 +0200

    text(): await connect in getUser function

--- src/pages/api/getUser.jsx
+++ src/pages/api/getUser.jsx
@@ -2,9 +2,9 @@ import connect from '../../lib/mongodb'
 import User from '../../model/userSchema'
 var cookie = require('cookie');
 
-connect()
-
 export default async function handler(req, res) {
+
+  await connect()
 
   const cookies = cookie.parse(req.headers.cookie || '')
   const email = cookies.userToken
